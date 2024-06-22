2ad619348f883e8bebbe8f8ee05645b3fdb99368
Sterlios <kuzmin.s.anton@gmail.com>
2024-06-22 23:33:07 +0200

    Create DB And table Users

---
+++ DBMigration/DatabaseCreator.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBMigration
+{
+    internal class DatabaseCreator : DbContext
+    {
+        private const string ServerName = "(localdb)\\mssqllocaldb";
+        private const string DatabaseName = "EnglishNinja";
+        private const string SchemaName = "dbo";
+        private const string ConnectionString = $"Server={ServerName};Database={DatabaseName};Trusted_Connection=True;";
+
+        public DatabaseCreator()
+        {
+            Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionString);
+        }
+    }
+}
---
+++ DBMigration/Migrations/20240622203456_InitialCreate.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DBMigration.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false),
+                    Name = table.Column<string>(type: "NVARCHAR(Max)", nullable: true)
+                },
+                constraints: table => table.PrimaryKey("PK_Users", user => user.Id));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
